183e47d8c1c158829863006158e2f183e44c4b33
Slothst <cnj991279@gmail.com>
2024-03-23 16:05:18 +0900

    init

---
+++ MovieReview/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MovieReview
+//
+//  Created by 최낙주 on 3/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
